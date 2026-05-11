a38e7ea205ac15d77e4ff52e05693cc09f3c9435
tpush <timofepushmin@mail.ru>
2026-05-11 20:41:21 +0500

    Update download.py

--- download.py
+++ download.py
@@ -2,29 +2,32 @@ import pandas as pd
 from sklearn.preprocessing import OrdinalEncoder
 
 def download_data():
-    url = "https://raw.githubusercontent.com/tpush/salary_train_pipe/refs/heads/main/job_salary_prediction_data.csv"
-    df = pd.read_csv(url)
-    df.to_csv("salary_data.csv", index=False)
-    print("Data downloaded successfully.")
+    url = 'https://raw.githubusercontent.com/tpush/salary_train_pipe/refs/heads/main/job_salary_prediction_data.csv'
+    df = pd.read_csv(url, delimiter = ',')
+    df.to_csv("salary.csv", index = False)
     return df
 
-def clear_data(path):
-    df = pd.read_csv(path)
+def clear_data(path2df):
+    df = pd.read_csv(path2df)
     
-    # удаляем строки с пустыми значениями в зарплате
+    # колонки для кодирования
+    cat_columns = ['experience_level', 'employment_type', 'job_title', 'employee_residence', 'company_location', 'company_size']
+    
+    # очистка данных
+    # удаляем пустые значения в целевой переменной
     df = df.dropna(subset=['salary_in_usd'])
     
-    # выбираем категориальные признаки для кодирования
-    cat_columns = ['experience_level', 'employment_type', 'job_title', 'company_size']
-    
-    # преобразуем текст в числа
+    # сбрасываем индексы и кодируем категории в числа
+    df = df.reset_index(drop=True)  
     ordinal = OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1)
-    df[cat_columns] = ordinal.fit_transform(df[cat_columns].astype(str))
+    ordinal.fit(df[cat_columns])
+    Ordinal_encoded = ordinal.transform(df[cat_columns])
+    df_ordinal = pd.DataFrame(Ordinal_encoded, columns=cat_columns)
+    df[cat_columns] = df_ordinal[cat_columns]
     
-    # сохраняем очищенный файл
     df.to_csv('df_clear.csv', index=False)
-    print("Data cleared and saved to df_clear.csv")
+    return True
 
 if __name__ == "__main__":
     download_data()
-    clear_data("salary_data.csv")
+    clear_data("salary.csv")
